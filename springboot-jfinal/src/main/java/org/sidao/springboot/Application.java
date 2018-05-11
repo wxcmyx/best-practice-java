aea0c6de1e28d41c31129aa66ce693aca1f03277
xiaochaoWang <wxcmyx@163.com>
2018-05-11 20:27:32 +0800

    加入融合

--- springboot-jfinal/src/main/java/org/sidao/springboot/Application.java
+++ springboot-jfinal/src/main/java/org/sidao/springboot/Application.java
@@ -11,6 +11,7 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Application.class);
         app.setBannerMode(Banner.Mode.OFF);//关闭banner
+        app.addListeners();
         app.run(args);
 
    }
--- springboot-jfinal/src/main/java/org/sidao/springboot/web/controller/SampleController.java
+++ springboot-jfinal/src/main/java/org/sidao/springboot/web/controller/SampleController.java
@@ -1,11 +1,15 @@
 package org.sidao.springboot.web.controller;
 
+import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.plugin.activerecord.Record;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @EnableAutoConfiguration
@@ -16,9 +20,10 @@ public class SampleController {
 
     @RequestMapping("/hello")
     @ResponseBody
-    String home() {
-//        db.queryForList("select count(*) from githubproject");
-        return  db.queryForList("select count(*) from githubproject").toString();
+    public String home() {
+        String countstr=  db.queryForList("select count(*) from githubproject").toString();
+        List<Record> countlist= Db.use("ds1").find("select count(*) from githubproject");
+        return countstr+"!!"+countlist.toString();
     }
 
 }
